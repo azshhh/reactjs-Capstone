ae42c906f23bad1ed98637c1914af0d742785100
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-11 15:19:34 +0530

    Creating layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,41 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <h1>Getting started...</h1>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 };
