c4e00a01d6690df49e4f2c17d97a519218dfcecd
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-14 16:58:35 +0530

    Connecting firebase with SignIn Component, logGoogleUser

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,16 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <h1>SignIn Page</h1>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+  return (
+    <div>
+      <h1>SignIn Page</h1>
+      <button onClick={logGoogleUser}>Sign-In with Google Popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
