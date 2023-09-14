0c4674ffecca865630da01d8642bd3e80cabb498
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-14 19:28:59 +0530

    SignUp Form setup

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,24 @@
+const SignUpForm = () => {
+  return (
+    <div>
+      <h1>SignUp with email and password</h1>
+      <form onSubmit={() => {}}>
+        <label>Name</label>
+        <input type="text" required />
+
+        <label>Email</label>
+        <input type="text" required />
+
+        <label>Password</label>
+        <input type="text" required />
+
+        <label>Confirm Password</label>
+        <input type="text" required />
+
+        <button type="subimit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,3 +1,4 @@
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import {
   createUserDocumentFromAuth,
   signInWithGooglePopup,
@@ -12,6 +13,7 @@ const SignIn = () => {
     <div>
       <h1>SignIn Page</h1>
       <button onClick={logGoogleUser}>Sign-In with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
