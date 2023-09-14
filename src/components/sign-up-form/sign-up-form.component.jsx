c292af8b99d9879f21d3d4ab12359b9eb8ce4f60
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-14 19:43:06 +0530

    SignUp Form handleChange

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,24 +8,55 @@ const defaultFormFields = {
 };
 
 const SignUpForm = () => {
-  const { formFields, setFormFields } = useState(defaultFormFields);
+  const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
-  
+
+  console.log(formFields);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
     <div>
       <h1>SignUp with email and password</h1>
       <form onSubmit={() => {}}>
         <label>Name</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
 
         <label>Email</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
 
         <label>Password</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
 
         <label>Confirm Password</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
 
         <button type="subimit">Sign Up</button>
       </form>
